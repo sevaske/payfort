b7b45244d9f855f27d1e36a07dac74366040ec7c
Sevak Mnatcakanian <sevachello@gmail.com>
2024-09-05 22:54:24 +0400

    Tests & Laravel 10+ #new_concept

--- config/payfort.php
+++ config/payfort.php
@@ -1,16 +1,10 @@
 <?php
 
 return [
-    // prod|sandbox
     'sandbox_mode' => env('PAYFORT_SANDBOX_MODE', true),
-    // enables debug mode for logging detailed request/response information
-    'debug_mode' => env('PAYFORT_DEBUG_MODE', false),
-    // channel used for logging debug information
     'log_channel' => env('PAYFORT_LOG_CHANNEL', 'stack'),
-    // language setting for the payfort api
+    'debug_mode' => env('PAYFORT_DEBUG_MODE', false),
     'language' => env('PAYFORT_LANGUAGE', 'en'), // en|ar
-
-    // configuration for merchants
     'merchants' => [
         'default' => [
             'merchant_identifier' => env('PAYFORT_MERCHANT_IDENTIFIER'),
@@ -26,6 +20,5 @@ return [
             'sha_response_phrase' => env('PAYFORT_APPLE_SHA_RESPONSE_PHRASE'),
             'sha_type' => env('PAYFORT_APPLE_SHA_TYPE', 'sha256'),
         ],
-        // multiple merchants can be added here
     ],
 ];
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,24 +1,16 @@
 <?php
 
-namespace Tests;
+namespace Sevaske\Payfort\Tests;
 
-use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Sevaske\Payfort\PayfortServiceProvider;
 
 class TestCase extends Orchestra
 {
-    protected function getPackageProviders($app): array
+    protected function getPackageProviders($app)
     {
         return [
             PayfortServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app): void
-    {
-        $app->useEnvironmentPath(__DIR__.'/..');
-        $app->bootstrapWith([LoadEnvironmentVariables::class]);
-        parent::getEnvironmentSetUp($app);
     }
 }
