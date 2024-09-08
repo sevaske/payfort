66280606731548e0085763542d022de6f6d23978
Sevak Mnatcakanian <sevachello@gmail.com>
2024-09-08 22:43:18 +0400

    Fix of ExampleTest.php #new_concept

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,14 +1,5 @@
 <?php
 
 it('can test', function () {
-
-    $response = app(\Sevaske\Payfort\Payfort::class)
-        ->merchant()
-        ->api()
-        ->checkStatus(merchantReference: '5000900')
-        ->getData();
-
-    dd($response);
-
     expect(true)->toBeTrue();
 });
